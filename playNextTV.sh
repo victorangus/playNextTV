16c0e547cdd04d43091898ce29e8bbf0cfbc872e
victorangus <victorangus@gmail.com>
2011-04-03 14:57:43 +0800

    修改播放方式與位址

--- playNextTV.sh
+++ playNextTV.sh
@@ -1,22 +1,47 @@
 #!/bin/sh
 
-player="vlc"
+#播放器
+player="mplayer"
 
-#800bits 640x360
-high="tvstreamv1_1@27911"
+#高畫質
+high="http://nextmedia-f.akamaihd.net/nexttvlive_1_600@49187"
 
-#400bits 640x360
-low="tvstreamv1_2@27911"
+#低畫質
+low="http://nextmedia-f.akamaihd.net/nexttvlive_1_300@49187"
 
-url="rtmp://cp93793.live.edgefcs.net/live/"
-app="live"
-swf="http://www.nexttv.com.tw/tv/nexttv/StrobeMediaPlayback.swf"
-page="http://www.nexttv.com.tw/"
-
+#檢查環境
 if ! which $player > /dev/null; then
-	echo "$player還沒安裝"
-elif ! which rtmpdump > /dev/null; then
-	echo "rtmpdump還沒安裝"
-else
-	rtmpdump -v -r $url -a $app -W $swf -p $page -y $high | $player -
+    echo "$player還沒安裝"
+    exit
+elif ! which curl > /dev/null; then
+    echo "curl還沒安裝"
+    exit
 fi
+
+#主程式
+echo "=================================================================="
+echo "     請選擇項目"
+echo "=================================================================="
+echo
+echo "(1)  = 高畫質            (2) = 低畫質                  (3) = 離開"
+echo
+printf "輸入數字後按<Enter>: "
+read Select
+
+case "$Select" in
+    # 高畫質
+    1)
+        curl $high | $player -
+    ;;
+
+    # 低畫質
+    2)
+        curl $low | $player -
+    ;;
+
+    # 離開
+    3)
+        exit
+    ;;
+    esac
+exit 0
